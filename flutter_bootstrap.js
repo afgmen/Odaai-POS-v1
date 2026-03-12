925b9192b61153e3e5bd8733cf88f33c80aa3375
Jin <jin@oda.vn>
2026-03-12 23:25:36 +0700

    deploy: cash quick-amount UX improvements (500K + accumulate + numpad)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1679315152" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2009786069" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
